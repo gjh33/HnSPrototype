fc55e0330686357bc210e0177e8a0e5ad15a71da
gjh33 <manhappylife@yahoo.ca>
2017-03-05 00:09:19 -0500

    Initial Commit - Click to Move
    Initial commit that has a basic click to move functionality

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    [Tooltip("Angle the camera is facing relative to parent.transform.forward")]
+    [Range(-360f, 360f)]
+    public float facingAngle;
+
+    [Tooltip("Distance from the character to the camera")]
+    public float distanceToCamera;
+
+    [Tooltip("Angle the camera is looking down on the player (bigger = closer to birds eye)")]
+    [Range(0f, 90f)]
+    public float viewingAngle;
+
+    [Tooltip("The target for the camera to follow")]
+    public GameObject target;
+	
+	// Update is called once per frame
+	void Update () {
+        // First set the position and rotation
+        gameObject.transform.rotation = Quaternion.identity;
+        gameObject.transform.Rotate(viewingAngle, facingAngle, 0);
+        Vector3 offset = new Vector3(0, 0, distanceToCamera);
+        gameObject.transform.position = target.transform.position - (transform.rotation * offset);
+    }
+}
---
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    [Tooltip("The speed at which the player moves")]
+    public float moveSpeed = 5;
+    [Tooltip("The rate the character turns (does not effect direction of movement)")]
+    public float turnSpeed = 15;
+    [Tooltip("Turns on debugging visuals")]
+    public bool debug = false;
+
+    // The position the player is currently trying to move to
+    private Vector3 targetPosition;
+    private Rigidbody myRigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+        targetPosition = gameObject.transform.position;
+        myRigidbody = gameObject.GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleInput();
+        UpdateMovement();
+        HandleDebug();
+	}
+
+    void HandleInput()
+    {
+        // If right click down
+        if (Input.GetMouseButton(1))
+        {
+            // Set the target position to mouse location projected onto game environment
+            MoveTargetToMousePosition();
+        }
+    }
+
+    void UpdateMovement()
+    {
+        // Find the direction we need to move to reach our goal
+        Vector3 moveDirection = (targetPosition - gameObject.transform.position).normalized;
+        // Set the x and z directions of velocity, but retain gravitation effects from y
+        myRigidbody.velocity = new Vector3(moveDirection.x * moveSpeed, myRigidbody.velocity.y, moveDirection.z * moveSpeed);
+    }
+
+    void MoveTargetToMousePosition()
+    {
+        // Create a ray from camera to mouse position
+        Ray mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        // Check for intersection with anything tagged Terrain and if so, set that as the target
+        RaycastHit hit;
+        if (Physics.Raycast(mouseRay, out hit))
+        {
+            if (hit.collider.tag == "Terrain")
+            {
+                targetPosition = hit.point;
+            }
+        }
+    }
+
+    void HandleDebug()
+    {
+        if (debug)
+        {
+            Debug.DrawLine(transform.position, targetPosition, Color.red);
+        }
+    }
+}
